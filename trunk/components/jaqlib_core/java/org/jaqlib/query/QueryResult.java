ee7344f8728e0e4c0d98c29ad1b56706cd656126
wernerfragner <wernerfragner@6575b88b-7747-0410-aca1-b8b7128698ac>
2008-12-28 09:31:44 +0000

    javadoc completed; javadoc gets deployed; version from 0.9 to 1.0-beta1

--- trunk/components/jaqlib_core/java/org/jaqlib/query/QueryResult.java
+++ trunk/components/jaqlib_core/java/org/jaqlib/query/QueryResult.java
@@ -7,6 +7,9 @@ import java.util.Set;
 import java.util.Vector;
 
 /**
+ * Provides methods to return the result of the query. It also provides methods
+ * to add additional where conditions.
+ * 
  * @author Werner Fragner
  * 
  * @param <T>
@@ -22,48 +25,104 @@ public class QueryResult<T, DataSourceType> extends
   }
 
 
+  /**
+   * @return a list containing the matching elements. If no matches have been
+   *         found then an empty list is returned.
+   */
   public List<T> toList()
   {
     return getQuery().getListResult();
   }
 
 
+  /**
+   * @return a vector containing the matching elements. If no matches have been
+   *         found then an empty vector is returned.
+   */
   public Vector<T> toVector()
   {
     return getQuery().getVectorResult();
   }
 
 
+  /**
+   * @return a set containing the matching elements. If no matches have been
+   *         found then an empty set is returned.
+   */
   public Set<T> toSet()
   {
     return getQuery().getSetResult();
   }
 
 
+  /**
+   * <p>
+   * Creates a map containing the matching elements. The keys for the elements
+   * are retrieved by reflection. The method that returns the key of the
+   * elements must be called on a 'dummy' object.
+   * </p>
+   * 
+   * <p>
+   * <b>Example:</b>
+   * 
+   * <pre>
+   * Account account = QB.getMethodCallRecorder(Account.class);
+   * Map&lt;String, Account&gt; results = QB.select(Account.class).from(accounts).toMap(
+   *     account.getId());
+   * </pre>
+   * 
+   * </p>
+   * 
+   * @param key the result of an method invocation on a 'dummy' element.
+   * @return a map containing the matching elements. If no matches have been
+   *         found then an empty map is returned.
+   */
   public <KeyType> Map<KeyType, T> toMap(KeyType key)
   {
     return getQuery().getMapResult(key);
   }
 
 
+  /**
+   * Basically the same as {@link #toMap(Object)}, but this method returns a
+   * {@link Hashtable} instead of a {@link Map}.
+   * 
+   * @return a hashtable containing the matching elements. If no matches have
+   *         been found then an empty hashtable is returned.
+   */
   public <KeyType> Hashtable<KeyType, T> toHashtable(KeyType key)
   {
     return getQuery().getHashtableResult(key);
   }
 
 
+  /**
+   * @return the unique result of the query. If more than one element matches
+   *         the query then an {@link IllegalStateException} is thrown. Returns
+   *         null if no match has been found.
+   * 
+   * @throws IllegalStateException if the query matches more than one element.
+   */
   public T uniqueResult()
   {
     return getQuery().getUniqueResult();
   }
 
 
+  /**
+   * @return the first element that matches the query. Returns null if no match
+   *         has been found.
+   */
   public T firstResult()
   {
     return getQuery().getFirstResult();
   }
 
 
+  /**
+   * @return the last element that matches the query. Returns null if no match
+   *         has been found.
+   */
   public T lastResult()
   {
     return getQuery().getLastResult();
@@ -71,11 +130,12 @@ public class QueryResult<T, DataSourceType> extends
 
 
   /**
-   * Simple condition that can be used to test all single elements for various
+   * Simple condition that can be used to test all elements for various
    * conditions.
    * 
    * @param <R> the result element type.
-   * @return
+   * @return an object that represents a WHERE condition on a single element of
+   *         the source collection.
    */
   public <R> SingleElementWhereCondition<T, DataSourceType, R> and()
   {
@@ -83,18 +143,41 @@ public class QueryResult<T, DataSourceType> extends
   }
 
 
+  /**
+   * Adds the given {@link WhereCondition} to the query using a AND connector.
+   * 
+   * @param condition
+   * @return an object to retrieve the result of the query or to add more WHERE
+   *         conditions to the query.
+   */
   public QueryResult<T, DataSourceType> and(WhereCondition<T> condition)
   {
     return getQuery().addAndWhereCondition(condition);
   }
 
 
+  /**
+   * Adds the given {@link WhereCondition} to the query using a OR connector.
+   * 
+   * @param condition
+   * @return an object to retrieve the result of the query or to add more WHERE
+   *         conditions to the query.
+   */
   public QueryResult<T, DataSourceType> or(WhereCondition<T> condition)
   {
     return getQuery().addOrWhereCondition(condition);
   }
 
 
+  /**
+   * Uses a recorded method call to test all elements for a specific condition.
+   * This condition can be specified in the returned
+   * {@link ComparableWhereCondition}. The condition is added to the query by
+   * using a AND connector.
+   * 
+   * @param evalResult the result of the recorded method call.
+   * @return an object to specify the condition.
+   */
   public <R> ComparableWhereCondition<T, DataSourceType, R> andMethodCallResult(
       R evalResult)
   {
@@ -102,6 +185,15 @@ public class QueryResult<T, DataSourceType> extends
   }
 
 
+  /**
+   * Uses a recorded method call to test all elements for a specific condition.
+   * This condition can be specified in the returned
+   * {@link ComparableWhereCondition}. The condition is added to the query by
+   * using a OR connector.
+   * 
+   * @param evalResult the result of the recorded method call.
+   * @return an object to specify the condition.
+   */
   public <R> ComparableWhereCondition<T, DataSourceType, R> orMethodCallResult(
       R evalResult)
   {
--- trunk/components/jaqlib_core/java/org/jaqlib/query/WhereClause.java
+++ trunk/components/jaqlib_core/java/org/jaqlib/query/WhereClause.java
@@ -2,6 +2,8 @@ package org.jaqlib.query;
 
 
 /**
+ * Represents the WHERE clause of the query.
+ * 
  * @author Werner Fragner
  * 
  * @param <T>
@@ -18,11 +20,12 @@ public class WhereClause<T, DataSourceType> extends
 
 
   /**
-   * Simple where clause that can be used to test all single elements for
-   * various conditions.
+   * Simple where clause that can be used to test all elements of the data
+   * source for various conditions.
    * 
    * @param <R> the result element type.
-   * @return
+   * @return an object that represents a where condition on a single element of
+   *         the data source.
    */
   public <R> SingleElementWhereCondition<T, DataSourceType, R> where()
   {
@@ -31,12 +34,13 @@ public class WhereClause<T, DataSourceType> extends
 
 
   /**
-   * Where clause that uses recorded method calls to test all elements for
-   * various conditions.
+   * Uses a recorded method call to test all elements for a specific condition.
+   * This condition can be specified in the returned
+   * {@link ComparableWhereCondition}. The condition is added to the query by
+   * using a AND connector.
    * 
-   * @param <R> the result element type.
-   * @param evalResult
-   * @return
+   * @param evalResult the result of the recorded method call.
+   * @return an object to specify the condition.
    */
   public <R> ComparableWhereCondition<T, DataSourceType, R> where(R evalResult)
   {
@@ -47,8 +51,9 @@ public class WhereClause<T, DataSourceType> extends
   /**
    * Where clause with user a defined condition.
    * 
-   * @param condition
-   * @return
+   * @param condition a not null condition.
+   * @return the result of the query (including methods to add other where
+   *         conditions).
    */
   public QueryResult<T, DataSourceType> where(WhereCondition<T> condition)
   {
