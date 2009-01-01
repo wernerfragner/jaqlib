58209bd50e0c78c3642360c626d2fd9cd7b1c655
wernerfragner <wernerfragner@6575b88b-7747-0410-aca1-b8b7128698ac>
2009-01-01 16:14:43 +0000

    added javadoc for Defaults

--- trunk/components/jaqlib_db/java/org/jaqlib/db/Defaults.java
+++ trunk/components/jaqlib_db/java/org/jaqlib/db/Defaults.java
@@ -7,13 +7,19 @@ import org.jaqlib.db.sql.typehandler.SqlTypeHandlerRegistry;
 import org.jaqlib.util.Assert;
 
 /**
- * Static helper class that holds default infrastructure component instances.
+ * Static helper class that holds default infrastructure component instances and
+ * global properties.<br>
+ * <b>NOTE: Changes to these components/properties have an effect on the entire
+ * application. Use with care!</b>
  * 
  * @author Werner Fragner
  */
 public class Defaults
 {
 
+  /**
+   * This class is not intended to be instantiated.
+   */
   private Defaults()
   {
     throw new UnsupportedOperationException();
@@ -33,56 +39,102 @@ public class Defaults
   }
 
 
+  /**
+   * Sets the default bean factory.<br>
+   * <b>NOTE: this method changes the default bean factory for the whole
+   * application! Use with care.</b>
+   * 
+   * @param beanFactory a not null bean factory.
+   */
   public static void setBeanFactory(BeanFactory beanFactory)
   {
     Defaults.beanFactory = Assert.notNull(beanFactory);
   }
 
 
+  /**
+   * @return the default bean mapping strategy.
+   */
   public static MappingStrategy getMappingStrategy()
   {
     return mappingStrategy;
   }
 
 
+  /**
+   * Sets the default bean mapping strategy.<br>
+   * <b>NOTE: this method changes the default bean mapping strategy for the
+   * whole application! Use with care.</b>
+   * 
+   * @param strategy a not null strategy.
+   */
   public static void setMappingStrategy(MappingStrategy strategy)
   {
     Defaults.mappingStrategy = Assert.notNull(strategy);
   }
 
 
+  /**
+   * @return the default Java type handler registry.
+   */
   public static JavaTypeHandlerRegistry getJavaTypeHandlerRegistry()
   {
     return javaTypeHandlerRegistry;
   }
 
 
-  public static void setJavaTypeHandlerRegistry(
-      JavaTypeHandlerRegistry javaTypeHandlerRegistry)
+  /**
+   * Sets the default Java type handler registry.<br>
+   * <b>NOTE: this method changes the default Java type handler registry for the
+   * whole application! Use with care.</b>
+   * 
+   * @param registry a not null registry.
+   */
+  public static void setJavaTypeHandlerRegistry(JavaTypeHandlerRegistry registry)
   {
-    Defaults.javaTypeHandlerRegistry = Assert.notNull(javaTypeHandlerRegistry);
+    Defaults.javaTypeHandlerRegistry = Assert.notNull(registry);
   }
 
 
+  /**
+   * @return the default SQL type handler registry.
+   */
   public static SqlTypeHandlerRegistry getSqlTypeHandlerRegistry()
   {
     return sqlTypeHandlerRegistry;
   }
 
 
-  public static void setSqlTypeHandlerRegistry(
-      SqlTypeHandlerRegistry sqlTypeHandlerRegistry)
+  /**
+   * Sets the default SQL type handler registry.<br>
+   * <b>NOTE: this method changes the default SQL type handler registry for the
+   * whole application! Use with care.</b>
+   * 
+   * @param registry a not null registry.
+   */
+  public static void setSqlTypeHandlerRegistry(SqlTypeHandlerRegistry registry)
   {
-    Defaults.sqlTypeHandlerRegistry = Assert.notNull(sqlTypeHandlerRegistry);
+    Defaults.sqlTypeHandlerRegistry = Assert.notNull(registry);
   }
 
 
+  /**
+   * Sets the default value for 'strictColumnCheck'.<br>
+   * <b>NOTE: this method changes the default value for the whole application!
+   * Use with care.</b>
+   * 
+   * @param strictColumnCheck
+   */
   public static void setStrictColumnCheck(boolean strictColumnCheck)
   {
     Defaults.strictColumnCheck = strictColumnCheck;
   }
 
 
+  /**
+   * @return true if all desired bean fields must be present in the result of
+   *         the SQL SELECT statement.
+   */
   public static boolean getStrictColumnCheck()
   {
     return strictColumnCheck;
