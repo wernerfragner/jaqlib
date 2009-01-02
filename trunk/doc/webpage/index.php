031cf9f5415117263f83eb24e5e8bf33d0606142
wernerfragner <wernerfragner@6575b88b-7747-0410-aca1-b8b7128698ac>
2009-01-02 17:50:29 +0000

    updated webpage docs

--- trunk/doc/webpage/index.php
+++ trunk/doc/webpage/index.php
@@ -1,11 +1,11 @@
 <?php
-header("Location: https://apps.sourceforge.net/mediawiki/jaqlib/");
+header("Location: http://apps.sourceforge.net/mediawiki/jaqlib/");
 exit; ?>
 
 <html>
  <head>
  </head>
  <body>
-  If you have not been automatically redirected, you should click <a href="https://apps.sourceforge.net/mediawiki/jaqlib/">here</a>.
+  If you have not been automatically redirected, you should click <a href="http://apps.sourceforge.net/mediawiki/jaqlib/">here</a>.
  </body>
 </html>
