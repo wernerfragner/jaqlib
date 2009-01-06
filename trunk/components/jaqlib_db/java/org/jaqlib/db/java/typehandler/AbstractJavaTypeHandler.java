52eec8690ad3bf84b77047c1c93e3a78b0c59ff1
wernerfragner <wernerfragner@6575b88b-7747-0410-aca1-b8b7128698ac>
2009-01-06 15:13:40 +0000

    made JavaTypeHandler implementations easier

--- trunk/components/jaqlib_db/java/org/jaqlib/db/java/typehandler/AbstractJavaTypeHandler.java
+++ trunk/components/jaqlib_db/java/org/jaqlib/db/java/typehandler/AbstractJavaTypeHandler.java
@@ -1,13 +1,27 @@
 package org.jaqlib.db.java.typehandler;
 
+import java.util.List;
+
+import org.jaqlib.util.CollectionUtil;
 import org.jaqlib.util.ReflectionUtil;
 
 /**
  * @author Werner Fragner
  */
-public abstract class AbstractJavaTypeHandler implements
-    JavaTypeHandler
+public abstract class AbstractJavaTypeHandler implements JavaTypeHandler
 {
+
+
+  public final Class<?>[] getSupportedTypes()
+  {
+    List<Class<?>> types = CollectionUtil.newDefaultList();
+    addSupportedTypes(types);
+    return types.toArray(new Class[types.size()]);
+  }
+
+
+  protected abstract void addSupportedTypes(List<Class<?>> types);
+
 
   protected RuntimeException handleIllegalInputValue(Object value,
       Class<?> requiredClass)
