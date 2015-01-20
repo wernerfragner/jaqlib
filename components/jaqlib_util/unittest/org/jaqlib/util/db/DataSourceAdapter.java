df8525c761aa0a2af528883bf9f7db641176b205
wernerfragner <wernerfragner@6575b88b-7747-0410-aca1-b8b7128698ac>
2015-01-20 20:16:21 +0000

    changes for JDK7

--- components/jaqlib_util/unittest/org/jaqlib/util/db/DataSourceAdapter.java
+++ components/jaqlib_util/unittest/org/jaqlib/util/db/DataSourceAdapter.java
@@ -3,6 +3,8 @@ package org.jaqlib.util.db;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
@@ -16,12 +18,14 @@ import javax.sql.DataSource;
 public class DataSourceAdapter implements DataSource
 {
 
+  @Override
   public Connection getConnection() throws SQLException
   {
     throw new UnsupportedOperationException();
   }
 
 
+  @Override
   public Connection getConnection(String username, String password)
       throws SQLException
   {
@@ -29,37 +33,50 @@ public class DataSourceAdapter implements DataSource
   }
 
 
+  @Override
   public PrintWriter getLogWriter() throws SQLException
   {
     throw new UnsupportedOperationException();
   }
 
 
+  @Override
   public int getLoginTimeout() throws SQLException
   {
     throw new UnsupportedOperationException();
   }
 
 
+  @Override
   public void setLogWriter(PrintWriter out) throws SQLException
   {
     throw new UnsupportedOperationException();
   }
 
 
+  @Override
   public void setLoginTimeout(int seconds) throws SQLException
   {
     throw new UnsupportedOperationException();
   }
 
 
+  @Override
   public boolean isWrapperFor(Class<?> iface) throws SQLException
   {
     throw new UnsupportedOperationException();
   }
 
 
+  @Override
   public <T> T unwrap(Class<T> iface) throws SQLException
+  {
+    throw new UnsupportedOperationException();
+  }
+
+
+  @Override
+  public Logger getParentLogger() throws SQLFeatureNotSupportedException
   {
     throw new UnsupportedOperationException();
   }
